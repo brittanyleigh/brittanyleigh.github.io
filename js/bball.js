706e0fbe39f4064b8ae46bec2d1e801741038507
Brittany <brittany.isenberg@gmail.com>
2018-06-03 19:31:53 -0400

    style updates

--- js/bball.js
+++ js/bball.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
   var data, scoreDate;
   var sfbtoa = 'britika:hYZvU4zN8jxw';
   var teamID = '131';
+  var daysOfTheWeek = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+  var months = ['Jan. ', 'Feb. ', 'March ', 'April ', 'May ', 'June ', 'July ', 'Aug. ', 'Sep. ', 'Oct. ', 'Nov. ', 'Dec. '];
   $('.teams').hide();
   date();
   todayAjax();
@@ -9,13 +11,16 @@ $(document).ready(function() {
   $('#selectTeam').change(function(){
     teamID = $(this).val();
     $('#heading img').attr("src", 'img/' + teamID + '.png');
+    $('body').removeClass().addClass('team-' + teamID);
     todayAjax();
     yesterdayAjax();
   });
 
   function date (){
     var today = new Date();
-    var todayDisplay = today.toDateString();
+    var yesterday = new Date();
+    yesterday.setDate(yesterday.getDate()-1);
+    var day = daysOfTheWeek[today.getDay()];
     var year = today.getFullYear();
     var month = today.getMonth() + 1;
     var date = today.getDate();
@@ -25,8 +30,9 @@ $(document).ready(function() {
     else {
       scoreDate = year + '' + month + '' + date-1;
     } 
+    var todayDisplay = day + '<br>' + months[month-1] + date + ', ' + year;
     // month is a single digit if jan-sep, api url requires MM format
-    $('#heading h1').append(today.toDateString());
+    $('#date').append(todayDisplay);
   }
   function todayAjax(){
     $.ajax
@@ -43,7 +49,7 @@ $(document).ready(function() {
         var dataSched = data.fullgameschedule.gameentry;
         if (dataSched){
           $('#today h2').html('Today\'s Game') // h2 is changed to plural for doubleheaders, this ensures it changes back to singular
-          todayGame (dataSched[0], '#today .game');
+          todayGame(dataSched[0], '#today .game');
         }
         else {
           $('#today .game .time h1').html('Off Day!');
@@ -99,12 +105,12 @@ $(document).ready(function() {
     var homeTeam = gameData.homeTeam.Name;
     var awayTeam = gameData.awayTeam.Name;
     var time = gameData.time;
-    var homeColor = teamColor(homeID);
-    var awayColor = teamColor(awayID);
-    var awayDiv = game + ' .teams .away';
-    var homeDiv = game + ' .teams .home';
-    changeBgColor(awayDiv, awayColor);
-    changeBgColor(homeDiv, homeColor);
+    var awayDiv = game + ' .teams .away h4';
+    var homeDiv = game + ' .teams .home h4';
+    var awayImg = game + ' .teams .away img';
+    $(awayImg).attr("src", 'img/' + awayID + '.png');
+    var homeImg = game + ' .teams .home img';
+    $(homeImg).attr("src", 'img/' + homeID + '.png');
     $(game + ' .teams').show();
     $(game + ' .time h1').html(time.slice(0, -2));
     $(awayDiv).html(awayTeam + ' @');
@@ -117,48 +123,20 @@ $(document).ready(function() {
     var awayTeam = gameData.game.awayTeam.Name;
     var homeScore = Number(gameData.homeScore);
     var awayScore = Number(gameData.awayScore);
-    var homeColor = teamColor(homeID);
-    var awayColor = teamColor(awayID);
-    var awayDiv = game + ' .teams .away';
-    var homeDiv = game + ' .teams .home';
+    var awayDiv = game + ' .teams .away h4';
+    var homeDiv = game + ' .teams .home h4';
+    var awayImg = game + ' .teams .away img';
+    $(awayImg).attr("src", 'img/' + awayID + '.png');
+    var homeImg = game + ' .teams .home img';
+    $(homeImg).attr("src", 'img/' + homeID + '.png');
     $(game + ' .teams').show();
-    changeBgColor(awayDiv, awayColor);
-    changeBgColor(homeDiv, homeColor);
-    $(awayDiv).html(awayTeam + ' : ' + awayScore);
-    $(homeDiv).html(homeTeam + ' : ' + homeScore);
+    $(awayDiv).html(awayTeam + ' <b>' + awayScore + '</b>');
+    $(homeDiv).html(homeTeam + ' <b>' + homeScore + '</b>');
     if (homeID == teamID && homeScore > awayScore || awayID == teamID && awayScore > homeScore){
       $(game + ' .WL h1').html('W');
       } // if
     else{
       $(game + ' .WL h1').html('L');
     } // else
-  }
-  function teamColor(id) {
-    var teamColorClass;
-    if (id == 140 || id == 130 || id == 113 || id == 135 || id == 116 || id == 124 || id == 129 || id == 133 || id == 126 ){
-      return 'red-team';
-    }
-    else if (id == 111 || id == 122 || id == 128 || id == 136){
-      return 'orange-team';
-    }
-    else if (id == 119 || id == 138 || id == 114){
-      return 'black-team';
-    }
-    else if (id == 117 || id == 134 || id == 120 || id == 127 || id == 139 || id == 123 || id == 115 || id == 121){
-      return 'navy-team';
-    }
-    else if (id == 131 || id == 118 || id == 137 || id == 112){
-      return 'blue-team';
-    }
-    else if (id == 125){
-      return 'green-team';
-    }
-    else if (id == 132){
-      return 'yellow-team';
-    }
-  }
-  function changeBgColor(teamDiv, newColor){
-    $(teamDiv).removeClass('red-team blue-team black-team green-team yellow-team orange-team navy-team');
-    $(teamDiv).addClass(newColor);
   }
 }); //document ready
