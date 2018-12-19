6bcd64021ee7b51f5f3fc3c5e21facd4fc8fe7f5
Brittany <brittany.isenberg@gmail.com>
2018-12-19 15:02:47 -0500

    add favicon; js logic for time of day; responsive tweaks

--- js/breathe.js
+++ js/breathe.js
@@ -1,6 +1,16 @@
 $(document).ready(function() {
   $('.toggle div').on('click', function(){
+    toggleDayNight();
+  });
+
+  var date = new Date();
+  var time = date.getHours();
+  if (time >= 7 && time <= 17){
+    toggleDayNight();
+  }
+
+  function toggleDayNight(){
     $('.container').toggleClass('night day');
     $('.toggle div').toggleClass('active');
-  })
+  }
 });
