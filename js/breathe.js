b83f3abccd5b4b2a2df459f033ceb6d4f1c3ae4f
Brittany <brittany.isenberg@gmail.com>
2019-01-13 14:58:18 -0500

    add rain animation

--- js/breathe.js
+++ js/breathe.js
@@ -9,4 +9,9 @@ $(document).ready(function() {
     $('.toggle div').removeClass('active');
     $(toggle).addClass('active');
   }
+
+  for (var i = 0; i <= 400; i++){
+    $('.raindrops').append('<span></span>');
+    console.log('ABC');
+  }
 });
