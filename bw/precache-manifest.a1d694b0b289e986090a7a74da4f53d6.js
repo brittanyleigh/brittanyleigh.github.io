24a919f9c6be6075381707b357c81e0a53ae7dcb
Brittany <brittany.isenberg@gmail.com>
2019-08-22 14:34:40 -0400

    add bw

---
+++ bw/precache-manifest.a1d694b0b289e986090a7a74da4f53d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "056e9a8686a43a9ccb91a32032934893",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee8ba6faba71145efef7",
+    "url": "/static/css/main.a82fdff2.chunk.css"
+  },
+  {
+    "revision": "97fca9cd1574e02fbdc3",
+    "url": "/static/js/2.1608a74c.chunk.js"
+  },
+  {
+    "revision": "ee8ba6faba71145efef7",
+    "url": "/static/js/main.9148a977.chunk.js"
+  },
+  {
+    "revision": "8718e3332b39c918590b",
+    "url": "/static/js/runtime~main.aa641d8f.js"
+  },
+  {
+    "revision": "42139d3e41fc12be05375e745d3879a6",
+    "url": "/static/media/gray-bg.42139d3e.jpg"
+  }
+]);
